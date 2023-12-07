8441d041e3f4f2394814ac1657572f6eb44093fe
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2023-12-07 15:17:59 +0300

    Widget setup

--- app/routes/widget.js
+++ app/routes/widget.js
@@ -1,6 +1,22 @@
 async function register(app, options)
 {
-    // TODO: /session/<%=session.id%>/widget
+    const GET_SCHEMA =
+    {
+        params:
+        {
+            type: "object",
+            required: [ "session_id" ],
+            additionalProperties: false,
+            properties:
+            {
+                "session_id": { $ref: "positive_int" }
+            }
+        }
+    };
+    app.get("/session/:session_id/widget", { schema: GET_SCHEMA, config: { access: "authorization" } }, async (req, res) =>
+    {
+        return res.render("widget.ejs");
+    });
 }
 
 import plugin from 'fastify-plugin';
