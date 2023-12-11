b3a21483930b80bd7a271fd6389811d9e1ab563e
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2023-12-11 22:42:08 +0300

    Fixes

--- database/scripts/setup/constraints.sql
+++ database/scripts/setup/constraints.sql
@@ -22,7 +22,7 @@ CREATE FUNCTION bonuses_update_check() RETURNS TRIGGER
 LANGUAGE plpgsql VOLATILE LEAKPROOF STRICT PARALLEL SAFE
 AS $$
 BEGIN
-	IF (NEW.winning IS NULL) THEN
+	IF (OLD.winning IS NOT NULL) AND (NEW.winning IS NULL) THEN
 		RAISE EXCEPTION SQLSTATE '23000' USING
 			MESSAGE = 'Winning update check constraint violation',
 			DETAIL = 'Unsetting winning is not allowed',
@@ -47,13 +47,13 @@ CREATE FUNCTION bonuses_delete_check() RETURNS TRIGGER
 LANGUAGE plpgsql VOLATILE LEAKPROOF STRICT PARALLEL SAFE
 AS $$
 BEGIN
-	IF (SELECT is_active FROM bonuses_view WHERE id = OLD.id) THEN
+	IF (SELECT is_active FROM bonuses_view WHERE id = OLD.id) AND (SELECT mode FROM sessions_view WHERE id = OLD.session_id) = 'START' THEN
 		RAISE EXCEPTION SQLSTATE '23000' USING
 			MESSAGE = 'Bonuses delete constraint violation',
 			DETAIL = 'Deleteing active bonuses is not allowed',
 			HINT = 'Please, fix your request';
 	END IF;
-	RETURN NEW;
+	RETURN OLD;
 END $$;
 
 CREATE TRIGGER bonuses_delete_trigger BEFORE DELETE ON bonuses
